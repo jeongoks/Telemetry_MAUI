1e9829b693623e03d4563982fa9c23926c1d6def
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2023-05-20 22:36:44 +0200

    Styling of the charts done, needing the Commands to update the shown graphs

--- Telemetry.App/MauiProgram.cs
+++ Telemetry.App/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using Telemetry.App.ViewModels;
+using Telemetry.App.Views;
 
 namespace Telemetry.App;
 
@@ -8,12 +11,16 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
+			.UseSkiaSharp(true)
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddTransient<ChartViewModel>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
---
+++ Telemetry.App/ViewModels/ChartViewModel.cs
@@ -0,0 +1,73 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+
+namespace Telemetry.App.ViewModels
+{
+    public partial class ChartViewModel : ObservableObject
+    {
+        public ISeries[] Series { get; set; } =
+        {
+            new LineSeries<double>
+            {
+                Name = "Humidity",
+                Values = new double[] { 42, 10, 50, 23, 16, 40, 49 },
+                Stroke = new SolidColorPaint(SKColors.DarkSlateBlue) { StrokeThickness = 4 },
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null,
+                LineSmoothness = 0,
+                TooltipLabelFormatter = (chartPoint) => $"{chartPoint.Context.Series.Name}: {chartPoint.PrimaryValue}"
+            },
+
+            new LineSeries<double>
+            {
+                Name = "Temperature",
+                Values = new double[] { 19.5, 20.4, 24.7, 16.5, 20.7, 30, 20.3 },
+                Stroke = new SolidColorPaint(SKColors.Red) { StrokeThickness = 4 },
+                Fill = null,
+                GeometryFill = null,
+                GeometryStroke = null,
+                LineSmoothness = 0,
+                TooltipLabelFormatter = (chartPoint) => $"{chartPoint.Context.Series.Name}: {chartPoint.PrimaryValue}"
+            }
+        };
+
+        public Axis[] YAxis { get; set; } = new Axis[]
+        {
+            new Axis
+            {
+                Name = "Reading values",
+                NamePaint = new SolidColorPaint(SKColors.Black),
+                NameTextSize = 40,
+                LabelsPaint = new SolidColorPaint(SKColors.Coral),
+                TextSize= 35
+            }
+        };
+
+        public Axis[] XAxis { get; set; } = new Axis[]
+        {
+            new Axis
+            {
+                Name = "Time stamps - in days",
+                NamePaint = new SolidColorPaint(SKColors.Black),
+                NameTextSize = 40,
+                Labels = new string[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" },
+                LabelsPaint = new SolidColorPaint(SKColors.Coral),
+                TextSize= 35
+            }
+        };
+
+        public SolidColorPaint LegendTextPaint { get; set; } = new SolidColorPaint
+        {
+            Color = SKColors.Black  
+        };
+    }
+}
